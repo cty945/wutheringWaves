09a2e44c8810b5216fd97a83b322f617dc5ac601
Tianyu Chen <tchen@exegy.com>
2024-06-06 10:23:58 -0500

    fix 2 bugs.

--- wutheringWave.py
+++ wutheringWave.py
@@ -1,4 +1,36 @@
 import numpy as np
+def postDataProcess(crit1Expectation):
+    crit1Expectation = [[['{:.2f}'.format(x) for x in item] for item in sublist] for sublist in crit1Expectation]
+    for rowIdx, earlyDiscard in enumerate(earlyDiscards):
+        print("Discard if no double crit after first " + str(earlyDiscard + 1))
+        for col in range(crit1DiscardChecks):
+            print("Discard if no crit after " + str(col + 1), crit1Expectation[rowIdx][col])
+        print("\n")
+    print(crit1Expectation)
+
+    data = [[[float(item[0]), float('{:.2f}'.format(float(item[1]) / 10)), float(item[2])] for item in sublist] for
+            sublist in crit1Expectation]
+
+    newData = [[0 for col in range(3)] for row in range(16)]
+    for row in range(len(data)):
+        for col in range(len(data[0])):
+            for type in range(3):
+                newData[row * 4 + col][type] = data[row][col][type]
+
+    for row in newData:
+        print(row, '\n')
+
+    # csv = ''
+    # csv += '2总1单,2总2单,2总3单,2总4单,3总1单,3总2单,3总3单,3总4单,4总1单,4总2单,4总3单,4总4单,5总1单,5总2单,5总3单,5总4单\n'
+
+    csv = '经验, 打孔/10, 胚子\n'
+    for row in newData:
+        for val in row:
+            csv += str(val) + ','
+        csv = csv[:len(csv) - 1]
+        csv += "\n"
+    with open('wutheringwave.csv', 'w') as file:
+        file.write(csv)
 
 EXP_REUSE = 0.75
 HOLE_REUSE = 0.3
@@ -15,6 +47,7 @@ targetAmount = 10000
 crit1DiscardChecks = 4
 
 earlyDiscards = [1, 2, 3, 4]
+# earlyDiscards = [4]
 
 crit1Expectation = [[-1 for col in range(4)] for row in range(len(earlyDiscards))]
 
@@ -56,8 +89,10 @@ for idx, earlyDiscard in enumerate(earlyDiscards):
                 holeTotal += 10 * (checkLevel + 1) - curHole
                 curExp = expAccu[checkLevel]
                 curHole = 10 * (checkLevel + 1)
+                curExp -= expAccu[checkLevel] * (1 - EXP_REUSE)
+                curHole -= 10 * (checkLevel + 1) * (1 - HOLE_REUSE)
 
-                if 0 in echo[discard_idx+1: checkLevel + 1] or 1 in echo[discard_idx+1: checkLevel + 1]:
+                if 0 in echo[:checkLevel + 1] and 1 in echo[:checkLevel + 1]:
                     doubleCRIT = True
                     expTotal += expAccu[4] - expAccu[checkLevel]
                     holeTotal += 50 - (10 * (checkLevel + 1))
@@ -72,37 +107,21 @@ for idx, earlyDiscard in enumerate(earlyDiscards):
         expExpected = sum(expHistory) / targetAmount
         holeExpected = sum(holeHistory) / targetAmount
         embryoExpected = sum(embryoHistory) / targetAmount
+
+        # print("expHistory:", expHistory)
+        # print("holeHistory:", holeHistory)
+        # print("embryoHistory:", embryoHistory)
+        # print("")
+        #
+        # print("expExpected:", expExpected)
+        # print("holeExpected:", holeExpected)
+        # print("embryoExpected:", embryoExpected)
+        # print("")
+        # print("")
+
         crit1Expectation[idx][discard_idx] = (
             # 'del1:' + str(discard_idx + 1) + ' del2:' + str(earlyDiscard + 1),
             expExpected, holeExpected, embryoExpected)
 
-crit1Expectation = [[['{:.2f}'.format(x) for x in item] for item in sublist] for sublist in crit1Expectation]
-for rowIdx, earlyDiscard in enumerate(earlyDiscards):
-    print("Discard if no double crit after first " + str(earlyDiscard + 1))
-    for col in range(crit1DiscardChecks):
-        print("Discard if no crit after " + str(col + 1), crit1Expectation[rowIdx][col])
-    print("\n")
-print(crit1Expectation)
 
-data = [[[float(item[0]), float('{:.2f}'.format(float(item[1])/10)), float(item[2])] for item in sublist] for sublist in crit1Expectation]
-
-newData = [[0 for col in range(3)] for row in range(16)]
-for row in range(len(data)):
-    for col in range(len(data[0])):
-        for type in range(3):
-            newData[row * 4 + col][type] = data[row][col][type]
-
-for row in newData:
-    print(row, '\n')
-
-# csv = ''
-# csv += '2总1单,2总2单,2总3单,2总4单,3总1单,3总2单,3总3单,3总4单,4总1单,4总2单,4总3单,4总4单,5总1单,5总2单,5总3单,5总4单\n'
-
-csv = '经验, 打孔/10, 胚子\n'
-for row in newData:
-    for val in row:
-        csv += str(val) + ','
-    csv = csv[:len(csv)-1]
-    csv += "\n"
-with open('wutheringwave.csv', 'w') as file:
-    file.write(csv)
+postDataProcess(crit1Expectation)
